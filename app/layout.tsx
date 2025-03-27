ef1bf1628cb2064bd0c7d789ef62fc2667670b19
Marius Lefter <160734137+mariussde@users.noreply.github.com>
2025-03-27 14:23:14 +0100

    ff

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
+  title: "Next.js and Supabase",
   description: "The fastest way to build apps with Next.js and Supabase",
 };
 
@@ -42,7 +42,7 @@ export default function RootLayout({
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>Next.js Supabase Starter</Link>
+                    <Link href={"/"}>Next.js Supabase</Link>
                     <div className="flex items-center gap-2">
                       <DeployButton />
                     </div>
